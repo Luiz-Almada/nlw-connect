3d50ea7cdb6fbc37d001107a71c6f73123370da2
Luiz Almada <luizalmada@yahoo.com>
2025-02-20 23:44:57 -0300

    feat: Implementa acesso ao backend utilizando Orval

--- src/app/(home)/subscription-form.tsx
+++ src/app/(home)/subscription-form.tsx
@@ -2,8 +2,10 @@
 
 import { Button } from '@/components/button'
 import { InputField, InputIcon, InputRoot } from '@/components/input'
+import { subscribeToEvent } from '@/http/api'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { ArrowRight, Mail, User } from 'lucide-react'
+import { useRouter, useSearchParams } from 'next/navigation'
 import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
@@ -15,6 +17,14 @@ const subscriptionSchema = z.object({
 type SubscriptionSchema = z.infer<typeof subscriptionSchema>
 
 export function SubscriptionForm() {
+  
+  // quando estiver usando 'use client'
+  //const { push } = useRouter()
+  // ou const router = useRouter()
+  const router = useRouter()
+
+  const searchParams = useSearchParams()
+
   const {
     register,
     handleSubmit,
@@ -24,10 +34,16 @@ export function SubscriptionForm() {
   })
 
   //function onSubscribe(data: FormData) {
-  function onSubscribe(data: SubscriptionSchema) {
+  //async function onSubscribe(data: SubscriptionSchema) {
+  async function onSubscribe({ name, email}: SubscriptionSchema) {
     //const values = Object.fromEntries(data.entries())
     //console.log(values)
-    console.log(data)
+    //console.log(data)
+
+    const referrer = searchParams.get('referrer')
+
+    const { subscriberId } = await subscribeToEvent({ name, email, referrer})
+    router.push(`/invite/${subscriberId}`)
   }
 
   return (
--- src/app/invite/[subscriberId]/page.tsx
+++ src/app/invite/[subscriberId]/page.tsx
@@ -4,13 +4,22 @@ import { Copy, Link } from 'lucide-react'
 
 import Image from 'next/image'
 
-import logo from '../../assets/logo.svg'
+import logo from '../../../assets/logo.svg'
 import { InviteLinkInput } from './invite-link-input'
 import { Ranking } from './ranking'
 import { Stats } from './stats'
 
-export default function InvitePage() {
-  const inviteLink = 'http://localhost:3000/invite/1297197291fjosf090eq'
+// No Next, apenas arquivos com nome Page.tsx tem acesso a 'params' da rota
+interface InvitePageProps {
+  params: Promise<{
+    subscriberId: string
+  }>
+}
+
+export default async function InvitePage(props: InvitePageProps) {
+  const { subscriberId } = await props.params
+
+  const inviteLink = `http://localhost:3333/invites/${subscriberId}`
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
       <div className="flex flex-col gap-10 w-full max-w-[550px]">
@@ -37,7 +46,8 @@ export default function InvitePage() {
           </div>
 
           <InviteLinkInput inviteLink={inviteLink} />
-          <Stats />
+          
+          <Stats subscriberId={subscriberId} />
         </div>
       </div>
 
